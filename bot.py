2684022acabab96a790ccfbf14d4fc6918753250
S.M.Shahriar Zarir <148766170+zarir94@users.noreply.github.com>
2024-07-02 05:49:49 +0000

    Replace

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
 import asyncio, random, time, traceback, sys
 
 TOKEN : Final[str] = '7467645069:AAE5NexNiCIIAxRVZ7xsHhq_JzHFh4jG_Uc'
-ID: Final[str] = '@test28372ndb'
+ID: Final[str] = '@AvaitorGreenSignalsbyReDHaT'
 BOT : Final[Bot] = Bot(TOKEN)
 c = [0]
 glt = lambda: c[0]
