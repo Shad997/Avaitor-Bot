7f46e9ef7f45e28149642795d9a82e7e931d580f
S.M.Shahriar Zarir <148766170+zarir94@users.noreply.github.com>
2024-08-08 17:47:52 +0000

    update timeout

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
 from typing import Final
 from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.request import HTTPXRequest
 
 
 TOKEN: Final[str] = '7467645069:AAE5NexNiCIIAxRVZ7xsHhq_JzHFh4jG_Uc'
-BOT: Final[Bot] = Bot(TOKEN)
+BOT: Final[Bot] = Bot(TOKEN, request=HTTPXRequest(
+    connection_pool_size=270,
+    read_timeout=140,
+    write_timeout=170,
+    connect_timeout=120,
+    pool_timeout=100,
+    media_write_timeout=140
+))
 
 
 def single_button(text, url):
